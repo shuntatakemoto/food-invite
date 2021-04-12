f490489e75503f56c2564812d64ca9ff1725e0b7
shuntatakemoto <rt80405@gmail.com>
2021-04-12 13:04:28 +0900

    styling

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -2,124 +2,49 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { auth, provider, storage } from "../firebase";
 
-import {
-  Avatar,
-  Button,
-  CssBaseline,
-  TextField,
-  Paper,
-  Grid,
-  Typography,
-  makeStyles,
-  Modal,
-  IconButton,
-  Box,
-} from "@material-ui/core";
+// import {
+//   Avatar,
+//   Button,
+//   CssBaseline,
+//   TextField,
+//   Paper,
+//   Grid,
+//   Typography,
+//   makeStyles,
+//   Modal,
+//   IconButton,
+//   Box,
+// } from "@material-ui/core";
 
-import SendIcon from "@material-ui/icons/Send";
-import CameraIcon from "@material-ui/icons/Camera";
-import EmailIcon from "@material-ui/icons/Email";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+// import SendIcon from "@material-ui/icons/Send";
+// import CameraIcon from "@material-ui/icons/Camera";
+// import EmailIcon from "@material-ui/icons/Email";
+// import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+// import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: "100vh",
-  },
-  image: {
-    backgroundImage:
-      "url(https://images.unsplash.com/photo-1613946069412-38f7f1ff0b65?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80)",
-    backgroundRepeat: "no-repeat",
-    backgroundColor:
-      theme.palette.type === "light"
-        ? theme.palette.grey[50]
-        : theme.palette.grey[900],
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+import LockOpenIcon from "@material-ui/icons/LockOpen";
 
 const Auth: React.FC = () => {
-  const classes = useStyles();
   const signInTwitter = async () => {
     await auth.signInWithPopup(provider).catch((err) => alert(err.message));
   };
 
   return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Button
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-              onClick={signInTwitter}
-            >
-              SignIn with Twitter
-            </Button>
-          </form>
-        </div>
-      </Grid>
-    </Grid>
+    <div className="text-center bg-yellow-100 flex justify-center items-center h-screen">
+      <div className="bg-white pb-10 h-2/4 m-10">
+        <p className="text-5xl p-10 font-bold">Sign In</p>
+        <p className="m-5">
+          サービスをご利用するにはTwitterでログインが必要です。
+        </p>
+        <button
+          className="bg-yellow-100 p-2 mt-5 rounded-2xl text-center"
+          onClick={signInTwitter}
+        >
+          <LockOpenIcon />
+          Sign In with Twitter
+        </button>
+      </div>
+    </div>
   );
 };
 
--- src/components/Mylist.tsx
+++ src/components/Mylist.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { auth } from "../firebase";
+import LockIcon from "@material-ui/icons/Lock";
 
-const Mylist = () => {
-  return <div>ログイン完了</div>;
+const Mylist: React.FC = () => {
+  return (
+    <div className="text-center">
+      <button
+        onClick={() => auth.signOut()}
+        className="bg-yellow-100 p-2 rounded-2xl"
+      >
+        <LockIcon />
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default Mylist;
