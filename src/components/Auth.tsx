0d47ec3f85b12a898092dd3f8722ee1650d23836
shuntatakemoto <rt80405@gmail.com>
2021-04-23 15:20:37 +0900

    デザインちょっと変更

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,27 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { auth, provider, storage } from "../firebase";
-
-// import {
-//   Avatar,
-//   Button,
-//   CssBaseline,
-//   TextField,
-//   Paper,
-//   Grid,
-//   Typography,
-//   makeStyles,
-//   Modal,
-//   IconButton,
-//   Box,
-// } from "@material-ui/core";
-
-// import SendIcon from "@material-ui/icons/Send";
-// import CameraIcon from "@material-ui/icons/Camera";
-// import EmailIcon from "@material-ui/icons/Email";
-// import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-// import AccountCircleIcon from "@material-ui/icons/AccountCircle";
-
 import LockOpenIcon from "@material-ui/icons/LockOpen";
 
 const Auth: React.FC = () => {
@@ -30,14 +9,14 @@ const Auth: React.FC = () => {
   };
 
   return (
-    <div className="text-center bg-yellow-100 flex justify-center items-center h-screen">
+    <div className="text-center bg-main-color flex justify-center items-center h-screen">
       <div className="bg-white pb-10 h-1/2 m-10">
         <p className="text-5xl p-10 font-bold">Sign In</p>
         <p className="m-5">
           サービスをご利用するにはTwitterでログインが必要です。
         </p>
         <button
-          className="bg-yellow-100 p-2 mt-5 rounded-2xl text-center"
+          className="bg-sub-color p-2 mt-5 rounded-2xl text-center"
           onClick={signInTwitter}
         >
           <LockOpenIcon />
