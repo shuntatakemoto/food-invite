3691a933bd0881b46eb861695831b7394e85d007
shuntatakemoto <rt80405@gmail.com>
2021-05-25 17:55:08 +0900

    feat:アイテム（店詳細ページ）削除機能を実装しました

--- src/components/templates/MyItem.tsx
+++ src/components/templates/MyItem.tsx
@@ -3,6 +3,10 @@ import { useSelector } from "react-redux";
 import { selectItem } from "../../features/itemSlice";
 import Button from "../atoms/Button";
 import DeleteIcon from "@material-ui/icons/Delete";
+import { db } from "../../firebase";
+import { useHistory } from "react-router-dom";
+import { selectPost } from "../../features/postSlice";
+import { selectUser } from "../../features/userSlice";
 interface PROPS {
   itemName: string;
   itemPostId: string;
@@ -14,11 +18,30 @@ interface PROPS {
 
 const MyItem: React.FC<PROPS> = (props) => {
   const item = useSelector(selectItem);
+  const history = useHistory();
+  const user = useSelector(selectUser);
+  const post = useSelector(selectPost);
+
+  const deleteItem = () => {
+    db.collection(user.uid)
+      .doc(post.postId)
+      .collection("restaurant")
+      .doc(item.itemPostId)
+      .delete()
+      .then(() => {
+        console.log("Document successfully deleted!");
+      })
+      .catch((error) => {
+        console.error("Error removing document: ", error);
+      });
+    history.push("/detail");
+  };
+
   return (
     <div className="flex-1">
       <p>MyItem.tsx</p>
       <p>この店を削除する</p>
-      <DeleteIcon fontSize="large" />
+      <DeleteIcon fontSize="large" onClick={deleteItem} />
       <p className="text-center">{item.itemName}</p>
       <p className="text-center">{item.itemMemo}</p>
       {/* <p>{item.itemPostId}</p> */}
