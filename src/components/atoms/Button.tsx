40679a179d5a072f02d03b684f6107e411aca28b
shuntatakemoto <rt80405@gmail.com>
2021-05-26 16:39:30 +0900

    fix:<a>タグから<Link>に変更しました（aタグだと毎回レンダリングされてしまうため）

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -9,12 +9,12 @@ interface PROPS {
 const Button: React.FC<PROPS> = (props) => {
   return (
     <div className="text-center my-5 ">
-      <a
-        href={props.buttonLink}
+      <Link
+        to={props.buttonLink}
         className="bg-black text-white font-bold py-2 px-5 rounded-full shadow-xl hover:bg-gray-400 hover:text-white "
       >
         <span className="text-lg font-light">{props.buttonText}</span>
-      </a>
+      </Link>
     </div>
   );
 };
