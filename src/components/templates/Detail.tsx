80ea5f2157025c44e99ef5c44bb3b560adeab9b2
shuntatakemoto <rt80405@gmail.com>
2021-06-17 18:10:21 +0900

    feat:シェアボタンを追加しました（見た目のみ）

--- src/components/templates/Detail.tsx
+++ src/components/templates/Detail.tsx
@@ -63,6 +63,7 @@ const Detail: React.FC<PROPS> = (props) => {
           buttonText="&emsp;&emsp;店を追加する&emsp;&emsp;"
           buttonLink="/add-List"
         />
+        <Button buttonText="&nbsp;リストをシェアする&nbsp;" buttonLink="" />
         {/* <Button buttonText="このリストを保存する" buttonLink="./add-List" /> */}
         {/* <Button buttonText="このリストを削除する" buttonLink="./add-List" /> */}
         <DeleteIcon fontSize="large" onClick={deleteList} />
