264c5f194c5b4a4d8c6f2e96598c566aa25cefc2
shuntatakemoto <rt80405@gmail.com>
2021-07-23 19:50:04 +0900

    feat:ログインしているかしていないかでヘッダーとフッターに表示させる内容を変更しました

--- src/components/molecules/Footer.tsx
+++ src/components/molecules/Footer.tsx
@@ -1,19 +1,33 @@
-import React, { useState } from "react";
+import React from "react";
 import { auth } from "../../firebase";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../features/userSlice";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Emoji } from "emoji-mart";
 
 const Footer: React.FC = () => {
   const user = useSelector(selectUser);
   return (
     <div className="bg-sub-color flex h-16 items-center justify-center">
-      <Link to="/">
-        <p className="px-4 text-xl mr-3">Home</p>
-      </Link>
-      <button onClick={() => auth.signOut()} className="px-4 text-xl ml-3">
-        Logout
-      </button>
+      {user.uid ? (
+        <Link to="/">
+          <p className="px-4 text-xl mr-3">Home</p>
+        </Link>
+      ) : (
+        <div className="mr-5">
+          <Emoji emoji="partying_face" size={32} />
+        </div>
+      )}
+
+      {user.uid ? (
+        <button onClick={() => auth.signOut()} className="px-4 text-xl ml-3">
+          Logout
+        </button>
+      ) : (
+        <div className="ml-5">
+          <Emoji emoji="drooling_face" size={32} />
+        </div>
+      )}
     </div>
   );
 };
--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../features/userSlice";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import LibraryAddIcon from "@material-ui/icons/LibraryAdd";
 import BookmarkIcon from "@material-ui/icons/Bookmark";
-import { useHistory, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { Emoji } from "emoji-mart";
 
 const Header: React.FC = () => {
   const user = useSelector(selectUser);
   const params = useParams() as any;
   const uid = params.uid as string;
-  // const createLink = `${user.uid}/create-list`;
   const createLink = `${user.uid}/create-list`;
 
   return (
@@ -18,16 +18,26 @@ const Header: React.FC = () => {
       <Link to="/">
         <h3 className="px-4 text-2xl">Food Invite</h3>
       </Link>
-      {/* if文で現在のパスが/detailの時、Linkを/add-listに変更させる */}
-      {/* <Link to="/:uid/create-list" className="mr-5 ml-5">
-        <LibraryAddIcon fontSize="large" />
-      </Link> */}
-      <Link to={createLink} className="mr-5 ml-5">
-        <LibraryAddIcon fontSize="large" />
-      </Link>
-      <Link to="/bookmark">
-        <BookmarkIcon fontSize="large" />
-      </Link>
+
+      {user.uid ? (
+        <Link to={createLink} className="mr-5 ml-5">
+          <LibraryAddIcon fontSize="large" />
+        </Link>
+      ) : (
+        <div className="mr-5 ml-5">
+          <Emoji emoji="knife_fork_plate" size={32} />
+        </div>
+      )}
+
+      {user.uid ? (
+        <Link to="/bookmark">
+          <BookmarkIcon fontSize="large" />
+        </Link>
+      ) : (
+        <div className="mr-5">
+          <Emoji emoji="yum" size={32} />
+        </div>
+      )}
     </div>
   );
 };
