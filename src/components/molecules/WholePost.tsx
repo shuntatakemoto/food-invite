9f4f935e9ab3c52988a5ef2144eb93fac85d9db7
shuntatakemoto <rt80405@gmail.com>
2021-07-28 15:09:36 +0900

    style: 色と文字の位置を変更しました

--- src/components/molecules/WholePost.tsx
+++ src/components/molecules/WholePost.tsx
@@ -27,7 +27,7 @@ const WholePost: React.FC<PROPS> = (props) => {
   const uid = params.uid;
 
   return (
-    <div className="bg-gray-200 rounded-lg shadow-xl overflow-hidden h-48 m-4 xl:m-6">
+    <div className=" rounded-lg shadow-xl overflow-hidden h-48 xl:h-60 m-4 xl:m-6">
       <Link
         to={`${uid}/${props.postId}`}
         onClick={() => {
@@ -36,10 +36,10 @@ const WholePost: React.FC<PROPS> = (props) => {
           dispatch(getEmojiName(props.emojiname));
         }}
       >
-        <div className="h-2/3 text-center pt-6">
+        <div className="h-36 xl:h-48 grid justify-items-center items-center bg-gray-300">
           <Emoji emoji={newEmojiName} size={64} set="twitter" />
         </div>
-        <div className="text-center h-1/3">
+        <div className="text-center h-12 xl:h-12 grid justify-items-center items-center bg-gray-200">
           <p className="text-base">{props.listname}</p>
         </div>
       </Link>
